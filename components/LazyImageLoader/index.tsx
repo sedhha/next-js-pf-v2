b86e00d731405a692ac230c593ceab26619b5c9c
sedhha <activity.schoolsh2@gmail.com>
2022-09-22 09:44:27 +0530

    Layout done

--- components/LazyImageLoader/index.tsx
+++ components/LazyImageLoader/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 interface LazyImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
-export default function LazyImageLoader({}: LazyImageProps) {
+export default function LazyImageLoader({ src, ...props }: LazyImageProps) {
+  const [visible, setVisible] = React.useState(false);
   return <div>LazyImageLoader</div>;
 }
--- pages/lazy-image.tsx
+++ pages/lazy-image.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from '@/styles/LazyImage.module.scss';
 
 type Props = {};
 
 export default function LazyImage({}: Props) {
-  return <div>LazyImage</div>;
+  return (
+    <div>
+      <div className={styles.WholePage}>Whole Screen</div>
+      <div className={styles.ImageContainer}>
+        <img src={'/images/image-01.jpg'} className={styles.Image} />
+        <img src={'/images/image-02.jfif'} className={styles.Image} />
+      </div>
+    </div>
+  );
 }
