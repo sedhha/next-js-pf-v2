1371c7eab4c9c61483e2b5c08281477e95c3821a
sedhha <activity.schoolsh2@gmail.com>
2022-10-15 23:13:24 +0530

    Overflow problem

--- components/HomePage/WorkSection/index.tsx
+++ components/HomePage/WorkSection/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import classes from './WorkSection.module.css';
 import SectionIds from '@/constants/sections.json';
+import ArrowIcon from '@/components/Svgs/ArrowIcon';
+import CardComponent from '@/components/Utility/Card';
 
 export default function WorkSection() {
 	return (
@@ -9,6 +11,18 @@ export default function WorkSection() {
 				<h1 className={classes.WorkExperienceHeader}>
 					Professional Career & Work Experience
 				</h1>
+			</div>
+			<div className={classes.ExperienceContainer}>
+				<div className={`${classes.NavigationButtonBox}`}>
+					<ArrowIcon height={60} width={30} />
+				</div>
+				<CardComponent />
+				<CardComponent />
+				<CardComponent />
+
+				<div className={`${classes.NavigationButtonBox}`}>
+					<ArrowIcon className={classes.RightArrow} height={60} width={30} />
+				</div>
 			</div>
 		</section>
 	);
--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,13 @@
 declare module '*module.css' {
-  const styles: {
-    [className: string]: string
-  }
-  export default styles
+	const styles: {
+		[className: string]: string;
+	};
+	export default styles;
+}
+
+declare module '*.svg' {
+	import React = require('react');
+	export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+	const src: string;
+	export default src;
 }
