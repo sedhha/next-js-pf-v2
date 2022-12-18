da7ca73d328f115d21d6d0b65ac1b2427ac56a85
sedhha <activity.schoolsh2@gmail.com>
2022-12-18 16:50:39 +0530

    Chat Responsivenes

--- components/HomePage/Projects/index.tsx
+++ components/HomePage/Projects/index.tsx
@@ -26,7 +26,6 @@ export default function WorkExperience() {
 	}, []);
 	return (
 		<section id={pageSections.PROJECTS} className="secition">
-			<br />
 			<div className={classes.IntroHeader}>
 				<h1 className={classes.IntroHeaderContent}>Cool things I did</h1>
 			</div>
