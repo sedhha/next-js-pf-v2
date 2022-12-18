85723711394bd7df7b63f685952bcfafcd6cb0d2
sedhha <activity.schoolsh2@gmail.com>
2022-12-18 19:38:26 +0530

    Minor FIxes

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -9,9 +9,7 @@ export default function Header() {
 	return (
 		<div className={classes.Header}>
 			<div className={classes.LogoBox}>
-				<h1 className={classes.Logo}>
-					Shivam <span className={classes.FocusWord}>Sahil</span>
-				</h1>
+				<h1 className={classes.Logo}>Shivam Sahil</h1>
 				<LazyImage className={classes.LogoImage} src={'/morpankh.svg'} />
 			</div>
 			<DarkModeToggle />
