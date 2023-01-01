aab51bcec89af7566712b3023a921ecdca6261c9
sedhha <activity.schoolsh2@gmail.com>
2023-01-01 19:05:18 +0530

    Updated Intro Page

--- components/HomePage/index.tsx
+++ components/HomePage/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useAppSelector } from '@/redux/hooks';
 import Header from '@/v2/Header';
+import Intro from '@/v2/Intro';
 
 export default function HomePage() {
 	const { darkMode } = useAppSelector((state) => state.navigation);
 	return (
 		<div className={darkMode ? 'darkMode' : 'lightMode'}>
 			<Header />
+			<Intro />
 		</div>
 	);
 }
