5b1f720b45169e804f75dda2aa6c9c20ec8d2fe0
sedhha <activity.schoolsh2@gmail.com>
2023-02-05 20:21:29 +0530

    Testing Recursion issue

--- components/BaseWrapper/utils.ts
+++ components/BaseWrapper/utils.ts
@@ -102,6 +102,7 @@ const maxRetriedConnections = async (
 	return new Promise((resolve) => {
 		setTimeout(() => {
 			const socketInReadyState = connection.readyState;
+			console.log({ socketInReadyState });
 			if (socketInReadyState) {
 				info(`ASW Connected`, connection.readyState);
 				resolve(connection);
