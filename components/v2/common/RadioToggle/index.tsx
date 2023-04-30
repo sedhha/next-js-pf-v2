cad4aba39546194148c333e852a0711351739097
sedhha <activity.schoolsh2@gmail.com>
2023-04-30 09:59:05 +0530

    Updated toggle Button

--- components/v2/common/RadioToggle/index.tsx
+++ components/v2/common/RadioToggle/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './RadioToggle.module.css';
 import { useAppSelector, useAppDispatch } from '@/redux/hooks';
 import { updateDarkMode } from '@/slices/navigation.slice';
+import { BsMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
 
 export default function DarkModeToggle() {
 	const dispatch = useAppDispatch();
@@ -9,7 +10,15 @@ export default function DarkModeToggle() {
 	const { darkMode } = useAppSelector((state) => state.navigation);
 	return (
 		<div className={classes.SwitchBoard} onClick={() => setOn(!darkMode)}>
+			<BsMoonStarsFill
+				className={`${classes.nightIcon}${
+					darkMode ? ' ' + classes.showSymbol : ''
+				}`}
+			/>
 			<section className={darkMode ? classes.off : classes.on} />
+			<BsFillSunFill
+				className={`${classes.dayIcon}${!darkMode ? ' ' + classes.showSymbol : ''}`}
+			/>
 		</div>
 	);
 }
---
+++ components/v3/README.md
@@ -0,0 +1,3 @@
+# V3 Upgrades
+
+Hello everyone. So in this phase, I am working on making asthetics of my web application a little more cleaner. I have started working on header.
